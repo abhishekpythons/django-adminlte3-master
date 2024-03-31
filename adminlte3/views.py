addf30b58b0cf75503b8f17a06a1e567d8994990
abhishekpythons <79434268+abhishekpythons@users.noreply.github.com>
2024-03-31 06:04:32 +0530

    adding functionalities like the example website

--- adminlte3/views.py
+++ adminlte3/views.py
@@ -13,6 +13,9 @@ from django.template.defaulttags import register
 def get_range(value):
     return range(value)
 
+@register.filter
+def get_range_matrix(matrix):
+    return range(len(matrix))
 
 @register.filter
 def get_range_from_1(value):
@@ -84,20 +87,8 @@ def criteria_count(request):
             criteria_names.append(c1)
             for i in range(2,criteria_count+1):
                 criteria_names.append(request.POST.get(f'criteria_{i}'))
-            print(criteria_names)
             return render(request, 'ahp_priority_values.html', {'criteria_count': criteria_count, 'criteria_names':criteria_names})
     return render(request, 'ahp_criteria_count.html', {'criteria_count':criteria_count})
-
-def calculate_ahp(request):
-    matrix = None
-    priority_vector = None
-    criteria_count = int(request.POST.get('criteria_count'))
-
-    if request.method == 'POST':
-        matrix = generate_ahp_matrix(request.POST, criteria_count)
-        priority_vector = calculate_priority_vector(matrix)
-
-    return render(request, 'ahp_results.html', {'matrix': matrix, 'priority_vector': priority_vector, 'criteria_count': criteria_count})
 
 def generate_ahp_matrix(post_data, criteria_count):
     matrix = np.ones((criteria_count, criteria_count))
@@ -105,10 +96,16 @@ def generate_ahp_matrix(post_data, criteria_count):
     for i in range(criteria_count):
         for j in range(i + 1, criteria_count):
             comparison_value = int(post_data.get(f'comparison_{i}_{j}'))
-            matrix[i, j] = comparison_value
-            matrix[j, i] = 1 / comparison_value
+            isAwrtB = True if post_data.get(f'isAwrtB_{i}_{j}') == 'True' else False
+            if isAwrtB:
+                matrix[i, j] = comparison_value
+                matrix[j, i] = 1 / comparison_value
+            else:
+                matrix[i, j] = 1 / comparison_value
+                matrix[j, i] = comparison_value
 
-    return matrix.tolist()
+
+    return matrix
 
 def calculate_priority_vector(matrix):
     eigvals, eigvecs = np.linalg.eig(matrix)
@@ -116,7 +113,32 @@ def calculate_priority_vector(matrix):
     priority_vector = eigvecs[:, max_eigval_index].real
     priority_vector /= np.sum(priority_vector)
     
-    return priority_vector.tolist()
+    return priority_vector
+
+def round_values(data, decimal_places):
+    if isinstance(data, list):
+        return [round(value, decimal_places) for value in data]
+    elif isinstance(data, np.ndarray):
+        return np.round(data, decimal_places)
+    else:
+        return round(data, decimal_places)
+
+def calculate_ahp(request):
+    matrix = None
+    priority_vector = None
+
+    if request.method == 'POST':
+        criteria_count = int(request.POST.get('criteria_count'))
+        criteria_names = request.POST.get('criteria_names')
+        criteria_names = ' '+criteria_names[1:-1]
+        criteria_names = [ele[2:-1] for ele in criteria_names.split(',')]
+        matrix = generate_ahp_matrix(request.POST, criteria_count)
+        priority_vector = calculate_priority_vector(matrix)
+        ranks = [criteria_count-list(np.argsort(priority_vector)).index(i) for i in range(criteria_count)]
+        priority_vector_percentage = 100*priority_vector
+        return render(request, 'ahp_results.html', {'matrix': round_values(matrix, 2), 'priority_vector': round_values(priority_vector_percentage, 2), 'criteria_count': criteria_count, 'criteria_names':criteria_names, 'ranks':ranks})
+    return(redirect('home'))
+
 
 def dashboard(request):
     a = 10
